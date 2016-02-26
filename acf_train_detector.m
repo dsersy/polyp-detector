d7b5c9765d0fb074af9e3036a88c1d21a900f6d5
Rok Mandeljc <rok.mandeljc@gmail.com>
2016-02-26 21:58:25 +0100

    Improved the ACF training script

--- acf_train_detector.m
+++ acf_train_detector.m
@@ -2,25 +2,33 @@ function acf_train_detector (varargin)
     parser = inputParser();
     parser.addParameter('data_dir', 'acf_data', @ischar);
     parser.addParameter('name', 'Polyp', @ischar);
+    parser.addParameter('use_negative_dir', false, @islogical);
     parser.parse(varargin{:});
-    
+
     data_dir = parser.Results.data_dir;
     name = parser.Results.name;
-    
+    use_negative_dir = parser.Results.use_negative_dir;
+
     %% Set up general options for training detector (see acfTrain)
     opts = acfTrain();
     opts.modelDs = [ 30, 30 ]; % Model height and width without padding
     opts.modelDsPad = [ 30, 30 ]; % Model height and width with padding
     opts.nWeak = [ 32, 128, 512, 2048 ]; % Number of weak classifiers per stage
-    opts.pBoost.pTree.fracFtrs = 1/16; % parameters for boosting (see adaBoostTrain.m) 
+    opts.pBoost.pTree.fracFtrs = 1/16; % parameters for boosting (see adaBoostTrain.m)
     opts.pLoad = {};
+
+    opts.nNeg       = 50000; % max number of neg windows to sample
+    opts.nPerNeg    = 100;  % max number of neg windows to sample per image
+    opts.nAccNeg    = 150000; % max number of neg windows to accumulate
 
     opts.posImgDir = fullfile(data_dir, 'train', 'pos'); % dir containing full positive images
     opts.posGtDir = fullfile(data_dir, 'train', 'posGT'); % dir containing ground truth
-    %opts.negImgDir = fullfile(data_dir, 'train', 'neg'); % dir containing negative images
+    if use_negative_dir,
+        opts.negImgDir = fullfile(data_dir, 'train', 'neg'); % dir containing negative images
+    end
 
     opts.winsSave = 1;
-    
+
     opts.name = name;
 
     %% Train
