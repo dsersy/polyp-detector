e0e763005f1847bf1ae66d2f54ff9ba5ae723fe5
Rok Mandeljc <rok.mandeljc@gmail.com>
2016-10-13 23:29:13 +0200

    Imported Jonkers-Volgenant Linear Assignment algorithm from Matlab Central.
    Will be used for point-wise evaluation.

--- startup.m
+++ startup.m
@@ -10,6 +10,9 @@ function startup ()
 
     %% LIBLINEAR
     addpath( fullfile(root_dir, 'external', 'liblinear', 'matlab') );
+
+    %% lapjv
+    addpath( fullfile(root_dir, 'external', 'lapjv') );
     
     %% CNN feature extractor
     run( fullfile(root_dir, 'external', 'cnn-feature-extractor', 'startup.m' ) );
