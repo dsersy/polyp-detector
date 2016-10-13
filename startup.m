0e79a8dda989f83f208ccc02d2d92377e0db0888
Rok Mandeljc <rok.mandeljc@fri.uni-lj.si>
2016-10-13 18:13:12 +0200

    Added liblinear to to startup.m

--- startup.m
+++ startup.m
@@ -1,10 +1,16 @@
-root_dir = fileparts(mfilename('fullpath'));
+function startup ()
+    % Root directory
+    root_dir = fileparts(mfilename('fullpath'));
 
-% This folder
-addpath(root_dir);
+    %% This folder
+    addpath(root_dir);
 
-% Piotr's toolbox
-addpath(genpath( fullfile(root_dir, 'external', 'piotr_toolbox') ));
+    %% Piotr's toolbox
+    addpath(genpath( fullfile(root_dir, 'external', 'piotr_toolbox') ));
 
-% CNN feature extractor
-run( fullfile(root_dir, 'external', 'cnn-feature-extractor', 'startup.m' ) );
+    %% LIBLINEAR
+    addpath( fullfile(root_dir, 'external', 'liblinear', 'matlab') );
+    
+    %% CNN feature extractor
+    run( fullfile(root_dir, 'external', 'cnn-feature-extractor', 'startup.m' ) );
+end
